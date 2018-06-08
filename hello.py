9df7ea36d13bec74af29febec5258fc6f890db1d
ThaeNgonPhu <39288747+ThaeNgonPhu@users.noreply.github.com>
2018-06-09 00:11:26 +0630

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def hello():
     return "Hello World"
 
-@app.route("/tiide")
-def tiide():
-    return "Welcome to TIIDE World"
+@app.route("/thae")
+def thae():
+    return "Hello my name is thae thae.Welcome to my page."
